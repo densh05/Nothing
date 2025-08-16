e29550b646beaf390a6212543cf330c2bed10f29
Denys Shaitan <schaitan.den@gmail.com>
2025-08-16 07:59:39 +0200

    nothing added

---
+++ Nothing3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nothing3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!Good morning 16 august");
+        }
+    }
+}
