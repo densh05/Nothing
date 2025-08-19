1059484cd08aefc49c399be45e9c356db76b8cb8
Denys Shaitan <schaitan.den@gmail.com>
2025-08-19 18:47:02 +0200

    Add information about me

--- Nothing3/Program.cs
+++ Nothing3/Program.cs
@@ -2,11 +2,46 @@
 
 namespace Nothing3
 {
-    internal class Program
+    class Information
     {
-        static void Main(string[] args)
+        private string index;
+        private string country;
+        private string city;
+        private string house;
+
+        public string Index { get { return index; } private set { index = value; } }
+        public string Country { get { return country; } private set { country = value; } }
+        public string City { get { return city; } private set { city = value; } }
+        public string House { get { return house; } private set { house = value; } }
+
+        public Information(string index, string country, string city, string house)
         {
-            Console.WriteLine("Hello World!Good evening 19 august");
+            Index = index;
+            Country = country;
+            City = city;
+            House = house;
         }
+
     }
+
+    class Program
+    {
+        static void Main()
+        {
+            Information information = new Information(
+                "2000",
+                "Denmark",
+                "Copenhagen",
+                "20");
+
+            Console.WriteLine(information.Index);
+            Console.WriteLine(information.Country);
+            Console.WriteLine(information.City);
+            Console.WriteLine(information.House);
+
+            Console.ReadKey();
+        }
+
+    }
+
 }
